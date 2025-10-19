885bd364c988b12a70e98e126f9456b63867335f
Marc <billpayingmarc@gmail.com>
2025-10-19 14:37:38 -0400

    logic

---
+++ app.js
@@ -0,0 +1,20 @@
+let celsius = 10;
+let fahrenheit = celsius * 1.8 + 32
+console.log(fahrenheit)
+
+
+
+let cash = 45   
+let price = 50
+let difference = cash - price
+
+if (cash > price) {
+    console.log ( `you paid extra - here's ${difference} dollars change` )
+}
+else if (cash === price) {
+    console.log ("you paid the exact amount, have a nice day!")
+}
+
+else {
+    console.log (`not enough money - you still owe ${difference * -1} dollars`)
+}
