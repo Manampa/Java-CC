1456693339aefa0318f3f7c5410b2a1a046b0ecf
Marc <billpayingmarc@gmail.com>
2025-10-20 12:34:18 -0400

    java

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
-let celsius = 10;
-let fahrenheit = celsius * 1.8 + 32
-console.log(fahrenheit)
+// let celsius = 10;
+// let fahrenheit = celsius * 1.8 + 32
+// console.log(fahrenheit)
 
 
 
-let cash = 45   
-let price = 50
-let difference = cash - price
 
-if (cash > price) {
-    console.log ( `you paid extra - here's ${difference} dollars change` )
-}
-else if (cash === price) {
-    console.log ("you paid the exact amount, have a nice day!")
-}
 
-else {
-    console.log (`not enough money - you still owe ${difference * -1} dollars`)
-}
+
+// let cash = 45   
+// let price = 50
+// let difference = cash - price
+
+// if (cash > price) {
+//     console.log ( `you paid extra - here's ${difference} dollars change` )
+// }
+// else if (cash === price) {
+//     console.log ("you paid the exact amount, have a nice day!")
+// }
+
+// else {
+//     console.log (`not enough money - you still owe ${difference * -1} dollars`)
+// }
+
+
+
+
+
+let hot = true
+
+hot? ''
+
+
+
+// 10:32 2.3 conditionals module 3
