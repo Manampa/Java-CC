b0af54b8409ccc24f28d97f073a1fbdc169855c4
Marc <billpayingmarc@gmail.com>
2025-11-07 09:19:27 -0500

    then clean way

--- app.js
+++ app.js
@@ -319,19 +319,35 @@
 
 
 
+// not clean way below
+
+// const emailRef = document.querySelector(".email");
+// console.log(emailRef)
+
+// fetch ("https://jsonplaceholder.typicode.com/users/1").then(response =>
+        //         response.json().then(data => {
+                //                 console.log(data)
+                //                 emailRef.innerHTML = data.email;
+                //         })
+                // )
+                
+                
+                
+                
+                
+                // clean way below 
+                
+                
 const emailRef = document.querySelector(".email");
-console.log(emailRef)
-
-
-
-fetch ("https://jsonplaceholder.typicode.com/users/1").then(response =>
-        response.json().then(data => {
-                console.log(data)
-                emailRef.innerHTML = data.name;
-        })
-)
-
-
+                
+fetch ("https://jsonplaceholder.typicode.com/users/1")
+                .then((response) => {
+                        return response.json();
+                })
+                .then((data) => {
+                        console.log(data);
+                        emailRef.innerHTML = data.email
+                });
 
 
 
