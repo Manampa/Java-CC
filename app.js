d0d85cf0f5b66d6d7a4c95b1a7b23e05b7bb8988
Marc <billpayingmarc@gmail.com>
2025-11-09 13:39:03 -0500

    promise

--- app.js
+++ app.js
@@ -339,20 +339,43 @@
                 
                 
 // const emailRef = document.querySelector(".email");
-                
+
 // fetch ("https://jsonplaceholder.typicode.com/users/1")
 // .then((response) => {
-//         return response.json();
-// })
-// .then((data) => {
-//         console.log(data);
-//         emailRef.innerHTML = data.email
-// });
+        //         return response.json();
+        // })
+        // .then((data) => {
+                //         console.log(data);
+                //         emailRef.innerHTML = data.email
+                // });
+   
+                
 
-async function  main() {
-       await fetch ("https://jsonplaceholder.typicode.com/users/1")
-        
+
+
+//                 const emailRef = document.querySelector(".email");
+// async function  main() {
+//        const response = await fetch ("https://jsonplaceholder.typicode.com/users/1")
+//        const data = await response.json()
+//        emailRef.innerHTML = data.email
+// }
+// main();
+
+
+
+
+
+
+
+const statusRef = document.querySelector(".status");
+
+function getSubscriptionStatus() {
+        return new Promise((resolve,reject) => {
+                setTimeout(() => {
+                        resolve("VIP");
+                }, 2000);
+                });
 }
 
 
-// 3.1 promises
+// 3.1 promises 22:30
